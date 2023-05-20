4c40882effe234d5a9d167a006b6b13f651f2f4f
catid <mrcatid@gmail.com>
2023-05-20 02:01:43 +0000

    wip

--- tiny_net.py
+++ tiny_net.py
@@ -60,7 +60,7 @@ class SRB(nn.Module):
         return x
 
 class tiny2x(nn.Module):
-    def __init__(self, d2sinput=True, rgb8output=True, channels=48, blocks=1):
+    def __init__(self, d2sinput=True, rgb8output=True, channels=32, blocks=1):
         super(tiny2x, self).__init__()
 
         self.input_convert = FromInputRGB8()
@@ -115,5 +115,6 @@ class tiny2x(nn.Module):
 def create_tiny2x(d2sinput, rgb8output):
     return tiny2x(
         channels=32,
+        blocks=1,
         d2sinput=d2sinput,
         rgb8output=rgb8output)
